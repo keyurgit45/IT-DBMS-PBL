e3368d7cf7109c51498496db07df9d8eea6c2fb6
Keyur Gandhi <keyur4tech123@gmail.com>
2022-04-26 23:41:14 +0530

    added validation

--- app/server/routes/user.py
+++ app/server/routes/user.py
@@ -1,12 +1,14 @@
+import re
 from typing import Optional
 from urllib3 import HTTPResponse
+from server.models.blog import ErrorResponseModel
 from server.models.blog import ResponseModel
 from server.auth.hashing import Hash
 from fastapi import  Cookie, FastAPI, HTTPException, Depends, Request, Response,status
 # from server.auth.oauth import get_current_user
 from server.auth.jwttoken import create_access_token
 from fastapi.security import OAuth2PasswordRequestForm
-
+from email_validator import validate_email, EmailNotValidError
 from server.models.user import User
 import motor.motor_asyncio
 
@@ -18,20 +20,28 @@ db = client.blogapp
 
 @userapp.post('/register' , tags=["User"])
 async def create_user(request:User):
-   hashed_pass = Hash.bcrypt(request.password)
-   user_object = dict(request)
-   user_object["password"] = hashed_pass
-   user_id = await db["users"].insert_one(user_object)
-   return {"res":"user created"}
+    user = await db["users"].find_one({"username":request.username})
+    if user:
+        return ErrorResponseModel("User already exist", 404, "Please enter unique username.")
+    try:
+        email = validate_email(request.email).email
+    except EmailNotValidError as e: 
+        print(str(e))
+        return {"message" : f"Email invalid! -> {str(e)}"}
+    hashed_pass = Hash.bcrypt(request.password)
+    user_object = dict(request)
+    user_object["password"] = hashed_pass
+    user_id = await db["users"].insert_one(user_object)
+    return {"res":"user created"}
 
 @userapp.post('/login' , tags=["User"])
 async def login(response: Response, request:OAuth2PasswordRequestForm = Depends() ):
     user = await db["users"].find_one({"username":request.username})
     if not user:
-       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return ErrorResponseModel("User does not exist", 404, "Please enter valid username.")
     ismatched =  Hash.verify(user["password"],request.password)
     if not ismatched:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return ErrorResponseModel("Password invalid", 404, "Please enter valid password.")
     access_token = create_access_token(data={"sub": user["username"] }, response=response)
     # response.set_cookie('_u',request.username , httponly=True, expires=30 * 60,secure=True)
     return {"access_token": access_token, "token_type": "bearer"}
